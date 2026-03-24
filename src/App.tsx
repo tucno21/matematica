a1cd52141bde7f905a8fe8484600962fab4a57cd
tucno21 <carlitostucno@gmail.com>
2026-03-23 20:30:34 -0500

    base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold">Matematic</h1>
+    </div>
+  )
+}
+
+export default App
